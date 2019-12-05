3b1b1c0325ed047f806396854b0174c444ee004c
SteveMustanski <steve.mustanski@gmail.com>
2019-12-05 12:56:31 -0500

    added get routes for standup and team

--- server/api/routes/standup.js
+++ server/api/routes/standup.js
@@ -2,6 +2,16 @@ const Standup = require('../../models/standup')
 
 module.exports =  function (router) {
   router.get('/standup', function (req, res) {
+    Standup.find()
+      .sort({ 'createdOn': 1})
+      .exec()
+      .then(docs => res.status(200)
+        .json(docs))
+      .catch(err => res.status(500)
+        .json({
+          message: 'Error finding standup notes',
+          error: err
+        }))
   })
   router.post('/standup', function (req, res) {
     let note = new Standup(req.body);
