386e61e6414677e054bc54e5b2f4b7a0b041cbfb
SteveMustanski <steve.mustanski@gmail.com>
2019-12-05 18:18:34 -0500

    added get roots for project

--- server/api/routes/project.js
+++ server/api/routes/project.js
@@ -1,8 +1,21 @@
 const Project = require('../../models/project');
 
 module.exports =  function (router) {
+  const qry = {
+    isActive: {$eq: true}
+  }
+
   router.get('/project', function (req, res) {
-    
+    Project.find(qry) 
+    .sort({ 'name': 1})
+    .exec()
+    .then(docs => res.status(200)
+      .json(docs))
+    .catch(err => res.status(500)
+      .json({
+        message: 'Error finding team members',
+        error: err
+      }))
   })
   router.post('/project', function (req, res) {
     let project = new Project(req.body);
--- server/api/routes/standup.js
+++ server/api/routes/standup.js
@@ -1,6 +1,9 @@
-const Standup = require('../../models/standup')
+const Standup = require('../../models/standup');
+const mongoose = require('mongoose');
 
 module.exports =  function (router) {
+
+  // get all standup notes
   router.get('/standup', function (req, res) {
     Standup.find()
       .sort({ 'createdOn': 1})
@@ -13,6 +16,23 @@ module.exports =  function (router) {
           error: err
         }))
   })
+  // get by team member
+  router.get('/standup/:teamMemberId', function (req, res) {
+    const qry = {
+      _teamMemberId: mongoose.Types.ObjectId(req.params.teamMemberId)
+    };
+    console.log(qry);
+    Standup.find(qry)
+      .sort({ 'createdOn': 1})
+      .exec()
+      .then(docs => res.Status(200)
+        .json(docs))
+      .catch(err => res.status(500)
+        .json({message: 'Error finding standup notes for the team member id', 
+               error: err}))
+
+  })
+  // add a stand up
   router.post('/standup', function (req, res) {
     let note = new Standup(req.body);
     note.save(function(err, note) {
